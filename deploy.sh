e19e8e97f4c8d1d746d4598401c4c30becd3cfd3
Matt <matt@Matts-MacBook-Pro.local>
2021-02-17 09:02:35 -0500

    first iteration of deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+
+##############################################################################
+##############################################################################
+##
+## MAGIC NUMBERS
+##
+##############################################################################
+##############################################################################
+
+
+ARG_NUMBER=1
+
+
+##############################################################################
+##############################################################################
+##
+## USAGE
+##
+##############################################################################
+##############################################################################
+
+
+usage(){
+    if [ $# -lt $ARG_NUMBER ]; then
+        echo "Usage: "
+        echo "$0 \ "
+        echo "  --env-file [ENV_FILE_PATH] \ "
+        exit 1
+    fi
+}
+
+
+##############################################################################
+##############################################################################
+##
+## PARSE ARGS
+##
+##############################################################################
+##############################################################################
+
+
+parse_args(){
+    while [[ $# > 1 ]];
+    do
+        key="$1"
+
+        case $key in
+            --env-file)
+            ENV_FILE="$2"
+            shift # past argument
+            ;;
+            *)
+            # unknown option
+            ;;
+        esac
+        shift
+    done
+}
+
+
+##############################################################################
+##############################################################################
+##
+## VALIDATE
+##
+##############################################################################
+##############################################################################
+
+validate_template(){
+    echo "[+] $(date) - Validating CloudFormation template ... $CF_TEMPLATE_PATH"
+    aws cloudformation validate-template \
+        --template-body 'file://'$CF_TEMPLATE_PATH
+}
+
+
+##############################################################################
+##############################################################################
+##
+## DEPLOY STACK
+##
+##############################################################################
+##############################################################################
+
+
+deploy_stack(){
+
+    ##
+    ## set CAPABILITY_NAMED_IAM to avoid possible
+    ## failures if IAM resources need to be created ... this setting
+    ## could be considered low security and removed if IAM resources
+    ## are not needed (the intention here is to genearlize)
+    ##
+    ## and setting CAPABILITY_AUTO_EXPAND to support use of nested
+    ## cloudformation modules
+    ##
+
+    echo "[+] $(date) - Deploying CloudFormation stack ... $STACK_NAME"
+    aws cloudformation create-stack \
+        --template-body 'file://'$CF_TEMPLATE_PATH \
+        --parameter 'file://'$CF_PARAM_PATH \
+        --stack-name $STACK_NAME \
+        --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+}
+
+
+##############################################################################
+##############################################################################
+##
+## WAIT on STACK CREATE
+##
+##############################################################################
+##############################################################################
+
+
+wait_stack_create(){
+    echo "[+] $(date) - Waiting for stack-create-complete ... $STACK_NAME"    
+    aws cloudformation wait stack-create-complete \
+        --stack-name $STACK_NAME
+}
+
+
+##############################################################################
+##############################################################################
+##
+## DESCRIBE EVENTS
+##
+##############################################################################
+##############################################################################
+
+
+describe_events(){
+    echo "[+] $(date) - Describing stack events ... $STACK_NAME"
+    aws cloudformation describe-stack-events \
+        --stack-name $STACK_NAME
+}
+
+
+##############################################################################
+##############################################################################
+##
+## MAIN
+##
+##############################################################################
+##############################################################################
+
+
+main(){
+    if ! validate_template; then
+        echo "[-] $(date) - Invalid CloudFormation template ... exiting"
+        return 1
+    else
+        deploy_stack
+        if ! wait_stack_create; then
+            echo "[-] $(date) - Stack create failed ... "
+            describe_events
+
+            ##
+            ## this scenario leaves an orphaned failed stack 
+            ## either manually teardown, or run the teardown.sh
+            ##
+            return 1
+
+        else
+            echo "[+] $(date) - Stack create complete ... "
+            describe_events
+            echo "[+] $(date) - SUCCESS! Stack deployment complete."
+            return 0
+        fi
+    fi
+}
+
+
+usage $@
+parse_args $@
+
+source $ENV_FILE
+main
+
