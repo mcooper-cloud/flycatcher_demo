03958172e5a52ba45321e8328e27c734e8a84c4c
Matt <matt@cloud>
2021-02-26 09:28:34 -0500

    jq testing in pipeline

--- pipeline/scripts/phase_2_prebuild.sh
+++ pipeline/scripts/phase_2_prebuild.sh
@@ -6,8 +6,6 @@ echo "[+] $(date) - Entered STAGE 1 PREBUILD - phase 2 prebuild"
 ENV_SH_PATH=$CODEBUILD_SRC_DIR/$ENV_PATH/$ENV_SH
 source $ENV_SH_PATH
 
-
-
 CF_JSON=$(
     aws cloudformation describe-stacks \
         --stack-name ${STACK_NAME}  \
@@ -15,18 +13,11 @@ CF_JSON=$(
         --output json
 )
 
-echo $AUTH0_CLIENT_ID_OUTPUT
-echo $AUTH0_CLIENT_SECRET_OUTPUT
-echo $AUTH0_DOMAIN_PARAM_OUTPUT
-
-jq
-
-#echo ${CF_JSON} | jq -rc '.[] | select(.OutputKey=="${AUTH0_CLIENT_ID_OUTPUT}") | .OutputValue '
-#AUTH0_CLIENT_ID_SM=$(echo ${CF_JSON} | jq -rc '.[] | select(.OutputKey=="${AUTH0_CLIENT_ID_OUTPUT}") | .OutputValue ')
-
-jq -rc '.[] | select(.OutputKey=="${AUTH0_CLIENT_ID_OUTPUT}") | .OutputValue ' <<< ${CF_JSON}
-AUTH0_CLIENT_ID_SM=$(jq -rc '.[] | select(.OutputKey=="${AUTH0_CLIENT_ID_OUTPUT}") | .OutputValue ' <<< ${CF_JSON})
+AUTH0_CLIENT_ID_SM=$(echo ${JSON_DATA} | jq --arg VAR ${AUTH0_CLIENT_ID_OUTPUT} -rc '.[] | select(.OutputKey==$VAR) | .OutputValue')
 echo $AUTH0_CLIENT_ID_SM
 
+AUTH0_CLIENT_SECRET_SM=$(echo ${JSON_DATA} | jq --arg VAR ${AUTH0_CLIENT_SECRET_OUTPUT} -rc '.[] | select(.OutputKey==$VAR) | .OutputValue')
+echo $AUTH0_CLIENT_SECRET_SM
 
-echo ${CF_JSON}
+AUTH0_DOMAIN_PARAM=$(echo ${JSON_DATA} | jq --arg VAR ${AUTH0_DOMAIN_PARAM_OUTPUT} -rc '.[] | select(.OutputKey==$VAR) | .OutputValue')
+echo $AUTH0_DOMAIN_PARAM
