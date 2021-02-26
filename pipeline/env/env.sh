d070925a3da5662033fbfcc20c8c40b93c2c9909
Matt <matt@cloud>
2021-02-26 08:30:14 -0500

    added Secrets Manager and Param Store variable retrieval to build process

--- pipeline/env/env.sh
+++ pipeline/env/env.sh
@@ -7,6 +7,19 @@ export_env(){
     export PHASE_START=$DATE_STRING
     echo "[+] $(date) PHASE_START: $PHASE_START"
 
+#    export AUTH0_CLIENT_ID_NAME="Auth0ClientID"
+#    export AUTH0_CLIENT_SECRET_NAME="Auth0ClientSecret"
+
+
+    export STACK_NAME="flycatcher-cicd-pipeline"
+    
+    ##
+    ## Cloudformation outputs used to dynamically retrieve Auth0 data
+    ##
+    export AUTH0_CLIENT_ID_OUTPUT="Auth0ClientIDARN"
+    export AUTH0_CLIENT_SECRET_OUTPUT="Auth0ClientSecretARN"
+    export AUTH0_DOMAIN_PARAM_OUTPUT="Auth0DomainParam"
+
 }
 
 export_env
--- pipeline/scripts/phase_2_prebuild.sh
+++ pipeline/scripts/phase_2_prebuild.sh
@@ -5,3 +5,14 @@ echo "[+] $(date) - Entered STAGE 1 PREBUILD - phase 2 prebuild"
 
 ENV_SH_PATH=$CODEBUILD_SRC_DIR/$ENV_PATH/$ENV_SH
 source $ENV_SH_PATH
+
+
+
+CF_JSON=$(
+    aws cloudformation describe-stacks \
+        --stack-name $STACK_NAME  \
+        --query "Stacks[0].Outputs" \
+        --output json
+)
+
+echo $CF_JSON
