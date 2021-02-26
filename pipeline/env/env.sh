d443e0db65a8a0e736b92227cb569de87e7d0c37
Matt <matt@cloud>
2021-02-26 10:33:35 -0500

    secrets management

--- pipeline/env/env.sh
+++ pipeline/env/env.sh
@@ -21,13 +21,13 @@ export_env(){
 function get_secret(){
     local __returnvar=$2
     local value=$(aws secretsmanager get-secret-value --secret-id $1 --query SecretString --output text)
+    echo $value
     eval $__returnvar="'$value'"
 }
 
 function get_parameter(){
     local __returnvar=$2
     local value=$(aws ssm get-parameter --name $1 --query Parameter.Value --output text)
-    echo $value
     eval $__returnvar="${value}"
 }
 
