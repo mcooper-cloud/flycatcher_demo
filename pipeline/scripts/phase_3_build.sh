3889d7cf5a916f09576a729243fb140f061d0c97
Matt <matt@cloud>
2021-03-01 13:43:45 -0500

    added Auth0 tenant deploy

--- pipeline/scripts/phase_3_build.sh
+++ pipeline/scripts/phase_3_build.sh
@@ -5,3 +5,28 @@ echo "[+] $(date) - Entered STAGE 1 PREBUILD - phase 3 build"
 
 ENV_SH_PATH=$CODEBUILD_SRC_DIR/$ENV_PATH/$ENV_SH
 source $ENV_SH_PATH
+
+##############################################################################
+##############################################################################
+##
+## deploy the Auth0 tenant
+##
+##############################################################################
+##############################################################################
+
+
+function auth0_deploy(){
+
+    cd $AUTH0_DEPLOY_PATH
+
+    export NODE_PATH=$(npm root -g)
+    export INPUT_FOLDER="./a0deploy"
+    export BASE_PATH="a0deploy"
+
+    echo "[+] Node version $(node --version)"
+    echo "[+] NPM version $(npm --version)"
+    npm start
+
+}
+
+auth0_deploy
