d3d392d7a571c0fd9192f05d68f8be3811e66c67
Matt <matt@cloud>
2021-03-10 07:31:02 -0500

    cleaned up parameters and naming conventions

--- env/cf/cloud_pipeline_env.sh
+++ env/cf/cloud_pipeline_env.sh
@@ -1,12 +1,31 @@
 
 export_env(){
 
+    ##########################################################################
+    ##########################################################################
+    ##
+    ## customize these lines
+    ##
+    ##########################################################################
+    ##########################################################################
+
+
+    export STACK_NAME="flycatcher-cicd-pipeline"
+    export REGION='us-east-1'
+
+
+    ##########################################################################
+    ##########################################################################
+    ##
+    ## DO NOT customize these lines
+    ##
+    ##########################################################################
+    ##########################################################################
+
+
     export WORKING_DIR=$(pwd)
     export INFRA_PATH='infra/cf'
     export PARAM_PATH='params/cf'
-    export REGION='us-east-1'
-
-    export STACK_NAME="flycatcher-cicd-pipeline"
     export CF_TEMPLATE_PATH=$INFRA_PATH'/pipeline/pipeline.json'
     export CF_PARAM_PATH=$PARAM_PATH'/cloud_pipeline_params.json'
 
--- env/cf/s3_staging_bucket_env.sh
+++ env/cf/s3_staging_bucket_env.sh
@@ -1,12 +1,31 @@
 
 export_env(){
 
+    ##########################################################################
+    ##########################################################################
+    ##
+    ## customize these lines
+    ##
+    ##########################################################################
+    ##########################################################################
+
+    export STACK_NAME="flycatcher-staging-bucket"
+    export REGION='us-east-1'
+
+
+    ##########################################################################
+    ##########################################################################
+    ##
+    ## DO NOT customize these lines
+    ##
+    ##########################################################################
+    ##########################################################################
+
+
     export WORKING_DIR=$(pwd)
     export INFRA_PATH='infra/cf'
     export PARAM_PATH='params/cf'
-    export REGION='us-east-1'
 
-    export STACK_NAME="flycatcher-staging-bucket"
     export CF_TEMPLATE_PATH=$INFRA_PATH'/s3/bucket.json'
     export CF_PARAM_PATH=$PARAM_PATH'/staging_bucket_params.json'
 
